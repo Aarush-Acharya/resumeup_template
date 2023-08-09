5bee643f6d077a167d5c9e600952bc5eb7ecd21a
Aarush Acharya <92685647+Aarush-Acharya@users.noreply.github.com>
2023-08-10 03:35:07 +0530

    fix: types

--- {{ cookiecutter.project_slug }}/lib/controller/resume_temp_controller.dart
+++ {{ cookiecutter.project_slug }}/lib/controller/resume_temp_controller.dart
@@ -10,10 +10,10 @@ class ResumeTempController extends GetxController {
   // Is presseds
   TextEditingController twitter_unme = TextEditingController(text: "{{ cookiecutter.twitter_uname }}");
   TextEditingController linkedIn_unme = TextEditingController(text: "{{ cookiecutter.linkedIn_uname }}");
-  bool Vercel_isPressed = {{ cookiecutter.is_vercel_active | lower }}.obs;
-  bool profile_isPressed = {{ cookiecutter.is_profile_active | lower }}.obs;
-  bool github_issues_isPressed = {{ cookiecutter.is_github_activity_active | lower}}.obs;
-  bool github_chart_isPressed = {{ cookiecutter.is_github_chart_active | lower}}.obs;
+  RxBool Vercel_isPressed = {{ cookiecutter.is_vercel_active | lower }}.obs;
+  RxBool profile_isPressed = {{ cookiecutter.is_profile_active | lower }}.obs;
+  RxBool github_issues_isPressed = {{ cookiecutter.is_github_activity_active | lower}}.obs;
+  RxBool github_chart_isPressed = {{ cookiecutter.is_github_chart_active | lower}}.obs;
   // .........................................
 
 //Data fetch variables
